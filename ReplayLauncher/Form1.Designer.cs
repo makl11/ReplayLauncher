9a01fcae03ead7e7e365a352b9cad4f81f89cd2b
Max Klingelhoeffer <max.klingelhoeffer@freenet.de>
2017-01-13 01:41:09 +0100

    Add Statusbar for debugging Add comparison of replay and game Add OpenFileDialog directly into Replay folder Improved Drag and Drop

--- ReplayLauncher/Form1.Designer.cs
+++ ReplayLauncher/Form1.Designer.cs
@@ -9,19 +9,23 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.button1 = new System.Windows.Forms.Button();
             this.replaypath = new System.Windows.Forms.TextBox();
+            this.lable1 = new System.Windows.Forms.StatusStrip();
+            this.label1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.groupBox1.SuspendLayout();
+            this.lable1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.replaypath);
-            this.groupBox1.Location = new System.Drawing.Point(12, 289);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.groupBox1.Location = new System.Drawing.Point(12, 272);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(260, 60);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Text = "Choose Replay:";
             // 
             // button1
             // 
@@ -29,7 +33,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Choose";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
@@ -40,12 +44,29 @@
             this.replaypath.Size = new System.Drawing.Size(166, 20);
             this.replaypath.TabIndex = 1;
             // 
+            // lable1
+            // 
+            this.lable1.GripMargin = new System.Windows.Forms.Padding(0);
+            this.lable1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.label1});
+            this.lable1.Location = new System.Drawing.Point(0, 339);
+            this.lable1.Name = "lable1";
+            this.lable1.Size = new System.Drawing.Size(284, 22);
+            this.lable1.TabIndex = 2;
+            this.lable1.Text = "statusStrip1";
+            // 
+            // label1
+            // 
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            // 
             // Form1
             // 
             this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 361);
+            this.Controls.Add(this.lable1);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -57,13 +78,18 @@
             this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.lable1.ResumeLayout(false);
+            this.lable1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox replaypath;
+        private System.Windows.Forms.StatusStrip lable1;
+        private System.Windows.Forms.ToolStripStatusLabel label1;
     }
 }
 
--- ReplayLauncher/Form1.cs
+++ ReplayLauncher/Form1.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +18,7 @@ namespace ReplayLauncher
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void Form1_DragEnter(object sender, DragEventArgs e)
@@ -28,13 +32,13 @@ namespace ReplayLauncher
 
             foreach (var file in files)
             {
-                if (System.IO.Path.GetExtension(file).Equals(".txt", StringComparison.InvariantCultureIgnoreCase))
+                if (System.IO.Path.GetExtension(file).Equals(".rofl", StringComparison.InvariantCultureIgnoreCase))
                 {
-                    MessageBox.Show("This is a text file", "Success!", MessageBoxButtons.OK, MessageBoxIcon.None);
+                    //run the replay
                 }
                 else
                 {
-                    MessageBox.Show("Not a text file", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show("This is no valid replay file", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
         }
@@ -43,11 +47,44 @@ namespace ReplayLauncher
 
         private void button1_Click_1(object sender, EventArgs e)
         {
+            RegistryKey regKey = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\\");
+            string mydocpath = (string)regKey.GetValue("Personal");
+            ofd.InitialDirectory = mydocpath + @"\League of Legends\Replays";
             ofd.Filter = "League of Legends Replay file | *.rofl";
             if (ofd.ShowDialog() == DialogResult.OK)
             {
                 replaypath.Text = ofd.FileName;
+                checkvers();
+                if (checkvers() == true)
+                {
+                    label1.Text = "Versions match";
+                }
+                if (checkvers() == false)
+                {
+                    label1.Text = "Versions do not match";
+                }
             }
         }
+
+        public bool checkvers()
+        {
+            //Reads version of the League of Legends.exe
+            string fileName = @"C:\Riot Games\League of Legends\RADS\solutions\lol_game_client_sln\releases\0.0.1.157\deploy\League of Legends.exe";
+            string gamevers = FileVersionInfo.GetVersionInfo(fileName).ProductVersion;
+            //Reads the Replayfile          !! NOT WORKING 100% !!
+            BinaryReader br = new BinaryReader(File.OpenRead(ofd.FileName));
+            br.BaseStream.Position = 0x14A;
+            char[] charArray = br.ReadChars(13);
+            string repvers = new string(charArray);
+            br.Close();
+            return (string.Equals(gamevers.Substring(0, 4), repvers.Substring(0, 4)));            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+ 
     }
 }
